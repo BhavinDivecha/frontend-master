24e9935379fe63142359a0c2ccd7a4fbed1073db
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2023-04-29 22:48:16 +0530

    Uploaded New Server Link

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     protocol: 'https://',
-    baseUrl: 'backend-master-production-28e2.up.railway.app/',
+    baseUrl: 'c4y.popaya.co.in:3001/',
     language: 'English',
     adminUrl: 'https://admin-cair4youth.vercel.app/',
   },
