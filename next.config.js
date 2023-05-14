9478946257bccd2507cf5cb6e69e0d50839e615e
Yash Jethva <yashjethva2504@gmail.com>
2023-05-14 16:29:38 +0530

    Config issues

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     protocol: 'https://',
-    baseUrl: 'da56-45-118-105-22.ngrok-free.app/',
+    baseUrl: 'c4y.popaya.co.in:3001/',
     language: 'English',
     adminUrl: 'https://admin-cair4youth.vercel.app/',
     country: 'default'
